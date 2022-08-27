df3dc38cd5122ebf54d97a29c539ac00569b6ba6
juruizgi <111092148+juruizgi@users.noreply.github.com>
2022-08-27 14:46:09 -0500

    modulo televisores

---
+++ televisores/tv.py
@@ -0,0 +1,82 @@
+class TV:
+  _numTV = 0
+  def __init__(self,marca,estado):
+    _canal = 1
+    _precio = 500
+    _volumen = 1
+    _control = None
+    self._marca = marca
+    self._estado = estado
+    TV._numTV+=1
+  
+  def setMarca(self,marca):
+    self._marca = marca
+  
+  def getMarca(self):
+    return self._marca
+  
+  def setControl(self,control):
+    self._control = control
+  
+  def getControl(self):
+    return self._control
+  
+  def setPrecio(self,precio):
+    self._precio = precio
+  
+  def getPrecio(self):
+    return self._precio
+  
+  def setVolumen(self,volumen):
+    if volumen>=0 and volumen<=7:
+      self._volumen = volumen
+
+  def getVolumen(self):
+    return self._volumen
+  
+  def setCanal(self,canal):
+    if canal<=120 and canal >=1 and self._estado ==True:
+      self._canal = canal
+    
+  def getCanal(self):
+    return self._canal
+  
+  @classmethod
+  def setNumTV(cls,numTV):
+    cls.numTV = numTV
+  
+  def turnOn(self):
+    self._estado = True
+  
+  def turnOff(self):
+    self._estado = False
+  
+  def getEstado(self):
+    return self._estado
+  
+  def canalUp(self):
+    if self._canal!=120 and self._estado == True:
+      self._canal+=1
+
+  def canalDown(self):
+    if self._canal!=1 and self._estado == True:
+      self._canal-=1
+  
+  def volumenUp(self):
+    if self._volumen!=7 and self._estado == True:
+      self._volumen+=1
+  
+  def volumenDown(self):
+    if self._volumen!=0 and self._estado == True:
+      self._volumen-=1
+
+
+if __name__ == "__main__":
+
+  tv1 = TV("sony",False)
+  print(tv1.getMarca())
+  print(tv1.getEstado())
+  control1 = Control()
+  control1.enlazar(tv1)
+  control1.turnOn()
+  print(tv1.getEstado())
